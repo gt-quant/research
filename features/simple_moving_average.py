865f612a1edc43f2292633ef4d03d2358196b65b
Carter Tegen <cartertegen@lawn-143-215-102-32.lawn.gatech.edu>
2024-10-31 19:12:33 -0400

    Updated utils for SMA

--- features/simple_moving_average.py
+++ features/simple_moving_average.py
@@ -1,18 +1,9 @@
 import numpy as np
-
+import utilities.utils as utils
 def simple_moving_average(df, symbol, moving_average_timeframe, return_timeframe):
     return_col = symbol + '__' + 'log_return__' + return_timeframe
 
-    if moving_average_timeframe == "1D":
-        window = 1440
-    elif moving_average_timeframe == "6H":
-        window = 360
-    elif moving_average_timeframe == "12H":
-        window = 720
-    elif moving_average_timeframe == "4D":
-        window = 5760
-    else:
-        raise Exception("Unknown timeframe.")
-
+    window = utils.parse_time(moving_average_timeframe)
+ 
     return df[return_col].rolling(window=window).mean()
 
