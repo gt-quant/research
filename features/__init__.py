f29a1d9f70f5dfb0f731f0c3dad402342f6a971a
Carter Tegen <cartertegen@lawn-143-215-102-32.lawn.gatech.edu>
2024-10-31 18:55:33 -0400

    Fixing feature for rsi

--- features/__init__.py
+++ features/__init__.py
@@ -1,3 +1,4 @@
 from .log_return import log_return
 from .gio_feature import gio_feature
 from .lin_return import lin_return
+from .rsi import rsi
--- features/edges.py
+++ features/edges.py
@@ -2,6 +2,7 @@ EDGES = {
     'log_return': ['open_price'],
     'lin_return': ['open_price'],
     'gio_feature': ['log_return__1M', 'close_price'],
+    'rsi': [],
     'momentum': ['open_price'],
     'simple_moving_average': ['log_return__6H', 'log_return__12H', 'log_return__1D', 'log_return__4D'],
     'z_score': ['open_price', 'simple_moving_average__6H', 'simple_moving_average__12H', 'simple_moving_average__1D', 'simple_moving_average__4D']
