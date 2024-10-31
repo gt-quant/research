098734bf3f99a92576f238719b2fbe4fc0a5ce7a
Akhil Kammila <akhilkammila@gmail.com>
2024-10-31 14:57:23 -0400

    added basic statarb feature + edge

--- features/edges.py
+++ features/edges.py
@@ -1,4 +1,5 @@
 EDGES = {
     'log_return': ['open_price'],
-    'gio_feature': ['log_return__1M', 'close_price']
+    'gio_feature': ['log_return__1M', 'close_price'],
+    'akhil_statarb': ['close_price']
 }
