0d74c7a221a47d3e4d4ed16b18a8ba1dd05bea63
Niteesh-Kalangi <niteesh.s.kalangi@gmail.com>
2024-10-31 20:27:14 -0400

    fixed feature calcs

--- features/z_score.py
+++ features/z_score.py
@@ -1,21 +1,13 @@
 import numpy as np
+from .simple_moving_average import simple_moving_average
+import utilities.utils as utils
 
 def z_score(df, symbol, moving_average_timeframe, return_timeframe):
     price_col = symbol + '__' + 'open_price'
-    moving_average_col = symbol + '__' + 'simple_moving_average__' + moving_average_timeframe
-
-    if moving_average_timeframe == "1D":
-        window = 1440
-    elif moving_average_timeframe == "6H":
-        window = 360
-    elif moving_average_timeframe == "12H":
-        window = 720
-    elif moving_average_timeframe == "4D":
-        window = 5760
-    else:
-        raise Exception("Unknown timeframe.")
+    moving_average_col = simple_moving_average(df, symbol, moving_average_timeframe, return_timeframe)
+    window = utils.parse_time(moving_average_timeframe)
 
 
-    return (df[price_col] - df[moving_average_col]) / df[price_col].rolling(window=window).std()
+    return (df[price_col] - moving_average_col) / df[price_col].rolling(window=window).std()
 
 
