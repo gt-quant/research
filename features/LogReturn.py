e4cf95ec1fbb8d1301a1376afb264a9f8529a359
Giovanni Hsu <giovannihsu.123@gmail.com>
2024-11-04 20:11:39 -0500

    slight modification

--- features/LogReturn.py
+++ features/LogReturn.py
@@ -3,21 +3,21 @@ import utilities.utils as utils
 
 from abstract_feature import AbstractFeature
 
-class GioNewFeature(AbstractFeature):
+class LogReturn(AbstractFeature):
     def __init__(self, symbol, timeframe):
 
         self.symbol = symbol
         self.timeframe = timeframe
 
         parents = [
-            symbol + '__' + 'open_price' + '__' + timeframe
+            'OpenPrice' + '__' + symbol
         ]
 
     def get_parents(self):
         return parents
 
     def get_feature(self, df):
-        price_col = self.symbol + '__' + 'open_price'
+        price_col = 'OpenPrice' + '__' + self.symbol
         shift_length = utils.parse_time(self.timeframe)
         return np.log(df[price_col] / df[price_col].shift(shift_length))
 
