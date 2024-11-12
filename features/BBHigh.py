9f6111b8ed6e0f83b40d5ed74ebaf3c7d75a66ee
Niteesh-Kalangi <niteesh.s.kalangi@gmail.com>
2024-11-11 20:39:52 -0500

    Added new features

---
+++ features/BBHigh.py
@@ -0,0 +1,27 @@
+import numpy as np
+import utilities.utils as utils
+import ta
+from .AbstractFeature import AbstractFeature
+
+class BBHigh(AbstractFeature):
+    def __init__(self, symbol, timeframe):
+
+        self.symbol = symbol
+        self.timeframe = timeframe
+
+        self.parents = [
+            'OpenPrice' + '__' + symbol
+        ]
+
+    def get_parents(self):
+        return self.parents
+
+    def get_feature(self, df):
+        price_col = 'OpenPrice' + '__' + self.symbol
+        # shift_length = utils.parse_time(self.timeframe)
+        window = utils.parse_time(self.timeframe)
+
+        bollinger = ta.volatility.BollingerBands(df[price_col], window=window)
+
+        return bollinger.bollinger_hband()
+
